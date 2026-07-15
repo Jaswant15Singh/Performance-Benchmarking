2fa5a845138efc40ead82ad09c6e7c4cfccfad58
Jaswant Singh <singhjaswant0932@gmail.com>
2026-07-15 18:55:43 +0100

    frontend home page setup

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(cors({origin:'http://localhost:5173'}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.use("/api", categoryRouter);
 app.use("/api", productRouter);
 
