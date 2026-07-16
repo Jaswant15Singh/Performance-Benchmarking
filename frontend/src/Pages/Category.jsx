55374289a0263b6cb6473b1fd9fa7e6d46381e18
Jaswant Singh <singhjaswant0932@gmail.com>
2026-07-16 15:25:51 +0100

    product add form

--- frontend/src/Pages/Category.jsx
+++ frontend/src/Pages/Category.jsx
@@ -1,9 +1,47 @@
 import NavbarComponent from "../Components/Navbar";
 import "../stylesheets/Home.css";
+import { useState, useEffect } from "react";
 const Category = () => {
+  const [category, setCategory] = useState([]);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    const controller = new AbortController();
+    async function fetchProducts() {
+      try {
+        const result = await fetch("http://localhost:3000/api/categories");
+        const data = await result.json();
+        setCategory(data.data);
+      } catch (error) {
+        console.log(error);
+        setError(error);
+      }
+    }
+    fetchProducts();
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
   return (
     <div className="page">
       <NavbarComponent />
+      <section id="products" className="container section">
+        <h3>All Categories</h3>
+        <div className="grid">
+          {category.map((p) => (
+            <div key={p.category_id} className="product-card">
+              <img
+                src={`http://localhost:3000/uploads/categories/${p.category_image}`}
+                width="100%"
+                style={{ aspectRatio: "1/1", cursor: "pointer" }}
+                alt={p.product_name}
+              />
+              <div className="product-category">{p.category_name}</div>
+              <div className="product-name">{p.category_name}</div>
+            </div>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,49 +1,80 @@
+import AddProductForm from "../Components/AddProduct";
 import NavbarComponent from "../Components/Navbar";
 import "../stylesheets/Home.css";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 const Product = () => {
-    const [products,setProducts]=useState([]);
-    const [error,setError]=useState(null);
-    useEffect(()=>{
-            const controller = new AbortController();
+  const [products, setProducts] = useState([]);
+  // const [error, setError] = useState(null);
+  const [refresh,setRefresh]=useState(false);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  useEffect(() => {
+    const controller = new AbortController();
 
-         async function fetchProducts() {
-           try {
-             const result = await fetch("http://localhost:3000/api/products");
-             const data = await result.json();
-             setProducts(data.data);
-           } catch (error) {
-             console.log(error);
-             setError(error);
-           }
-         }
-         fetchProducts();
-         return()=>{
-            controller.abort();
-         }
-    },[])
+    async function fetchProducts() {
+      try {
+        const result = await fetch("http://localhost:3000/api/products");
+        const data = await result.json();
+        setProducts(data.data);
+      } catch (error) {
+        console.log(error);
+        setError(error);
+      }
+    }
+    fetchProducts();
+    return () => {
+      controller.abort();
+    };
+  }, [refresh]);
 
-   
   return (
-    <div className="page">
-      <NavbarComponent />
-      <section id="products" className="container section">
-        <h3>Popular products</h3>
-        <div className="grid">
-          {products.map((p) => (
-            <div key={p.product_id} className="product-card">
-              <img src={`http://localhost:3000/uploads/products/${p.product_image}`} width="100%" style={{aspectRatio:"1/1",cursor:"pointer"}} alt={p.product_name} />
-              <div className="product-category">{p.product_name}</div>
-              <div className="product-name">{p.product_name}</div>
-              <div className="product-row">
-                <span className="product-price">{p.product_price}</span>
-                <button className="btn btn-small">Add</button>
-              </div>
-            </div>
-          ))}
+    <>
+      {isFormOpen && (
+        <div className="overlay">
+          <AddProductForm isFormOpen={isFormOpen} setIsFormOpen={setIsFormOpen} refresh={refresh} setRefresh={setRefresh}/>
         </div>
-      </section>
-    </div>
+      )}
+      <div className="page ">
+        <NavbarComponent />
+        <section
+          id="products"
+          className="container section"
+          style={{ position: "relative" }}
+        >
+          <h3>All products</h3>
+          <button
+            style={{
+              position: "absolute",
+              top: "30px",
+              right: "10px",
+              borderRadius: "5px",
+              background: "#58def1",
+              padding: "3px 10px",
+            }}
+            onClick={()=>{setIsFormOpen(!isFormOpen)}}
+          >
+            Add Product
+          </button>
+          <div className="grid">
+            {products.map((p) => (
+              <div key={p.product_id} className="product-card">
+                <img
+                  src={`http://localhost:3000/uploads/products/${p.product_image}`}
+                  width="100%"
+                  style={{ aspectRatio: "1/1", cursor: "pointer" }}
+                  alt={p.product_name}
+                />
+                <div className="product-category">{p.product_name}</div>
+                <div className="product-name">{p.product_name}</div>
+                <div className="product-row">
+                  <span className="product-price">{p.product_price}</span>
+                  <button className="btn btn-small">Add</button>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
+    </>
   );
 };
 export default Product;
