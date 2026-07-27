e40cbc17c0d992a098a3009b711b504fbbc64438
Jaswant Singh <singhjaswant0932@gmail.com>
2026-07-27 20:48:45 +0100

    testing ecs deployment

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const product = require("../controller/products.js");
 const {upload} = require("../middlewares/multer.js");
-
 const router = express.Router();
-
 router.get("/products", product.getProducts);
 router.post("/product",upload.single("product_image"),product.addProduct);
 router.put("/product/:id",upload.single("product_image"),product.updateProduct)
