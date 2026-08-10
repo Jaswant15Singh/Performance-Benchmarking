a7e38bb99f3649aed25e924ebb9fc42a1d063bca
Jaswant Singh <singhjaswant0932@gmail.com>
2026-08-10 19:20:35 +0100

    updating connection string of real time database

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const cors=require('cors');
-// require("dotenv").config();
-// const categoryRouter=require("./routes/category");
-// const productRouter=require("./routes/products.js")
+require("dotenv").config();
+const categoryRouter=require("./routes/category");
+const productRouter=require("./routes/products.js")
 const app = express();
 app.use(cors({origin:'*'}));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
-// app.use("/api", categoryRouter);
-// app.use("/api", productRouter);
+app.use("/api", categoryRouter);
+app.use("/api", productRouter);
 
-app.use('/test',(req,res)=>{
-    res.send("ttest is working")
-})
-app.use("/api/products",(req,res)=>{
-    res.json({success:"trues"});
-})
+// app.use('/test',(req,res)=>{
+//     res.send("ttest is working")
+// })
+// app.use("/api/products",(req,res)=>{
+//     res.json({success:"trues"});
+// })
 app.use((err,req,res,next)=>{    
     res.status(500).json({success:false, message:err.message})
 })
